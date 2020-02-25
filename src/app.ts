560cc2248be9ad054c9677aab062bbc4b48a6e39
Richard Pinheiro <richardpinheiro1992@gmail.com>
2020-02-24 22:51:57 -0300

    init tests

--- src/app.ts
+++ src/app.ts
@@ -5,28 +5,28 @@ import mongoose from 'mongoose'
 import routes from './routes'
 
 class App {
-  public express: express.Application
+    public express: express.Application
 
-  public constructor () {
-    this.express = express()
+    public constructor () {
+        this.express = express()
 
-    this.middlewares()
-    this.database()
-    this.routes()
-  }
+        this.middlewares()
+        this.database()
+        this.routes()
+    }
 
-  private middlewares (): void {
-    this.express.use(express.json())
-    this.express.use(cors())
-  }
+    private middlewares (): void {
+        this.express.use(express.json())
+        this.express.use(cors())
+    }
 
-  private database (): void {
-    mongoose.connect(`mongodb://${process.env.DB_HOST}:27017/tsexample`, { useNewUrlParser: true })
-  }
+    private database (): void {
+        mongoose.connect(process.env.DB_CONNECTION_STRING, { useNewUrlParser: true })
+    }
 
-  private routes (): void {
-    this.express.use(routes)
-  }
+    private routes (): void {
+        this.express.use(routes)
+    }
 }
 
 export default new App().express
